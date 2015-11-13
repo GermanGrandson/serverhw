a4bdac03c64220d56b4332a77c6c85f21786c260
German Grandson <ganieto1@gmail.com>
2015-11-13 14:42:01 -0500

    adding ssh files

--- question3.rb
+++ question3.rb
@@ -4,20 +4,34 @@ require 'nokogiri'
 
 get "/" do
 
-# def decimals(number)
-#     sprintf ('%.2f' % number)
-# end
+def decimals(number)
+    sprintf ('%.2f' % number)
+end
 
 # response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
 # dom = Nokogiri::HTML(response.body)
 # appl_stock = dom.xpath("//span[@id='yfs_l84_aapl']").first
+# "The current price for AL is $#{appl_stock}"
+
+
+response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+dom = JSON.parse(response.body)
+appl_stock = dom[]("//span[@id='yfs_l84_aapl']").first
+"The current price for AL is $#{appl_stock}"
+
+def dec
+
+  "Hello"
+
+end
+end
+
+
+
+#TRYING WITH TEXT
+# response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+# dom = Nokogiri::HTML(response.body)
+# appl_stock = dom.xpath("//span[@id='yfs_b00_aapl']").first
 # appl_stock = appl_stock.text
 # puts "The current price for AL is $#{appl_stock}"
 # end
-
-response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
-dom = Nokogiri::HTML(response.body)
-appl_stock = dom.xpath("//span[@id='yfs_b00_aapl']").first
-appl_stock = appl_stock.text
-puts "The current price for AL is $#{appl_stock}"
-end
